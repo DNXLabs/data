0fc11fa11c02801ccf8251cd2da241474643d40d
Arthur Diniz <arthurbdiniz@gmail.com>
2020-09-12 10:22:15 -0300

    Adds option to set endpoint_url for boto3
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- dnxdata/resource.py
+++ dnxdata/resource.py
@@ -6,14 +6,15 @@ _session = boto3.session.Session()
 region_name = _session.region_name
 
 AWS_REGION = os.getenv("AWS_REGION", region_name)
+ENDPOINT_URL = os.getenv("ENDPOINT_URL", None)
 
 # S3
-s3_client = boto3.client('s3', region_name=AWS_REGION)
-s3_resource = boto3.resource('s3', region_name=AWS_REGION)
+s3_client = boto3.client('s3', region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
+s3_resource = boto3.resource('s3', region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
 
 # Dynamodb
-dynamo_client = boto3.client('dynamodb', region_name=AWS_REGION)
-dynamo_resource = boto3.resource('dynamodb', region_name=AWS_REGION)
+dynamo_client = boto3.client('dynamodb', region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
+dynamo_resource = boto3.resource('dynamodb', region_name=AWS_REGION, endpoint_url=ENDPOINT_URL)
 
 # Glue
 glue_client = boto3.client("glue", region_name=AWS_REGION)
